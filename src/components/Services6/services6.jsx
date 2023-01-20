0994dccdf5a87c5b727623cb272beebd2d5a1319
Tsee <bbattseren88@gmail.com>
2023-01-20 11:42:00 +0800

    +fix

--- src/components/Services6/services6.jsx
+++ src/components/Services6/services6.jsx
@@ -11,19 +11,12 @@ const Services6 = () => {
             style={{ backgroundImage: 'url(/img/arch/s5.jpg)' }}
           >
             <h6 className="numb">01</h6>
-            <h5 className="h-25">Design</h5>
+            <h5 >Design</h5>
             <p className="demo-1">
               TextTrinm Taking a holistic approach, we contextualize prevailing
               best practice, and seek to offer solutions which are
               location-specific and obviate environmental damage
             </p>
-            <Link
-              to="/about/about-dark"
-              className="custom-font more main-color mt-10"
-              style={{ pointerEvents: 'none' }}
-            >
-              Read More
-            </Link>
           </div>
 
           <div
@@ -31,20 +24,13 @@ const Services6 = () => {
             style={{ backgroundImage: 'url(/img/arch/s5.jpg)' }}
           >
             <h6 className="numb">02</h6>
-            <h5 className="h-25">Urban Design</h5>
+            <h5 >Urban Design</h5>
             <p className="demo-1">
               The construction process is carbon intensive, and ensuring green
               buildings are not created at the expense of the environment
               elsewhere, requires intelligent identification of country-specific
               materials and suppliers.
             </p>
-            <Link
-              to="/about/about-dark"
-              className="custom-font more main-color mt-10"
-              style={{ pointerEvents: 'none' }}
-            >
-              Read More
-            </Link>
           </div>
 
           <div
@@ -52,18 +38,11 @@ const Services6 = () => {
             style={{ backgroundImage: 'url(/img/arch/s5.jpg)' }}
           >
             <h6 className="numb">03</h6>
-            <h5 className="h-25">Energy</h5>
+            <h5 >Energy</h5>
             <p className="demo-1">
               We benefit from an increasingly wide selection of potential
               renewable power options which can be applied to buildings.
             </p>
-            <Link
-              to="/about/about-dark"
-              className="custom-font more main-color mt-10"
-              style={{ pointerEvents: 'none' }}
-            >
-              Read More
-            </Link>
           </div>
 
           <div
@@ -71,21 +50,11 @@ const Services6 = () => {
             style={{ backgroundImage: 'url(/img/arch/s5.jpg)' }}
           >
             <h6 className="numb">04</h6>
-            <h5 className="h-25"> Social impact</h5>
+            <h5 > Social impact</h5>
             <p className="demo-1">
               Development can have an immediate effect on local employment,
-              engendering positive sentiment toward clients. With exacting
-              standards, scope for skills uplift, training and fair working
-              conditions, it can have profoundly positive implications in
-              casting the relationship between foreign and local entities.
+              engendering positive sentiment toward clients.
             </p>
-            <Link
-              to="/about/about-dark"
-              className="custom-font more main-color mt-10"
-              style={{ pointerEvents: 'none' }}
-            >
-              Read More
-            </Link>
           </div>
 
           <div
@@ -93,18 +62,11 @@ const Services6 = () => {
             style={{ backgroundImage: 'url(/img/arch/s5.jpg)' }}
           >
             <h6 className="numb">05</h6>
-            <h5 className="h-25">Enduring structures</h5>
+            <h5 >Enduring structures</h5>
             <p className="demo-1">
               In each building we develop, careful consideration is given to
               maintenance and renewal on a recurring basis.
             </p>
-            <Link
-              to="/about/about-dark"
-              className="custom-font more main-color mt-10"
-              style={{ pointerEvents: 'none' }}
-            >
-              Read More
-            </Link>
           </div>
         </div>
       </div>
